2e00d96c9961660ea96a578aa7698eaabcb42112
meryem08 <meriem.mansouri08@gmail.com>
2023-09-29 08:32:45 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+// Write your algorithm here
+  function hasTargetSum(array, target) {
+    const seenNumbers = new Set(); // Initialize an empty Set
+    for (const number of array) {
+      const complement = target - number;
+  
+      // .has returns true if the Set includes the complement
+      if (seenNumbers.has(complement)) return true;
+  
+      // .add adds the number to the Set
+      seenNumbers.add(number);
+    }
+    return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  - Time complexity: O(n)
 */
 
 /* 
   Add your pseudocode here
+
+ -create an empty Set called seenNumbers
+  iterate over the array of numbers
+ - for the current number, calculate its complement
+ - check if seenNumbers includes the complement
+ -if yes, return true
+ -add the current number to seenNumbers
+ -if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+
+  We start by creating an empty Set called seenNumbers to keep track of the numbers we have seen so far. We then iterate over the input array of numbers. For each number, we calculate its complement by subtracting it from the target number.
+
+We check if the Set seenNumbers already contains the complement. If it does, we return true because we have found a pair of numbers that adds up to the target.
+
+If the complement is not in the Set, we add the current number to the Set to keep track of it. We continue this process for all numbers in the array.
+
+If we reach the end of the array without finding a pair that adds up to the target, we return false.
+
+This solution has a time complexity of O(n) because we iterate through the array once, and the Set allows for efficient constant-time lookups.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +59,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
